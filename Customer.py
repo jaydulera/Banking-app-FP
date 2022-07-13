f83cacc1b014193dfb0cbd9ba4325a0b14e5b7ae
Jay Dulera <53439144+jaydulera@users.noreply.github.com>
2022-07-13 10:26:07 +0530

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -14,6 +14,7 @@ class Customer:
         self.totalBalance = totalBalance
 
     def getTotalBalance(self):
+        """Returns total balance of customer across all accounts"""
         if len(self.accounts) == 0:
             return 0
         balance = 0
@@ -23,35 +24,42 @@ class Customer:
 
     @staticmethod
     def createCustomer(firstName , lastName):
+        """Created new customer and returns created object"""
         Customer._customerID += 1
         customer = Customer(Customer._customerID , firstName , lastName , [] , getTotalBalance())
         Customer.listOfCustomers.append(customer)
         return customer
     
     def createAccount(self , fullName , bankAbbreviation):
+        """Creates new account and returns created object"""
         account = Account.createAccount(fullName , bankAbbreviation)
         if account:
             print("Account successfully created")
             self.accounts.append(account)
 
     def findAccount(self , accountNumber):
+        """Returns required account"""
         for account in self.accounts:
             if account.accountNumber == accountNumber:
                 return account
 
     def deposit(self , amount , accountNumber):
+        """Deposits required amount to said account"""
         Account.credit(amount , accountNumber)
     
     def withraw(self , amount , accountNumber):
+        """Withraws required amount from said account"""
         Account.debit(amount , accountNumber)
 
     @staticmethod
     def transfer(amount , recepientNumber , benefactorNumber):
+        """Transfers required amount to said recepient"""
         if Account.findAccount(recepientNumber):
             if Account.debit(amount , benefactorNumber):
                 Account.credit(amount , recepientNumber)
 
     def getBalance(self , accountNumber):
+        """Returns balance of said account"""
         account = findAccount(accountNumber)
         if not account:
             print("Account does not exist")
